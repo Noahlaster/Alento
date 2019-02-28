0d39cb634bf928e4f89fb4c6ba67a64e83b96d8f
Noahlaster <jhonnata38@hotmail.com>
2019-02-28 00:12:57 -0300

    -

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   // Create the browser window.
-  let win = new BrowserWindow({ width: 800, height: 600 })
+  let win = new BrowserWindow({ width: 800, height: 600, show: false})
 
   // and load the index.html of the app.
   win.loadFile('index.html')
   
+  //função responsavél por abrir a aplicação sem a tela branca.
+  win.once("ready-to-show",()=>{
+    win.show();
+  })
+
 }
 
 app.on('ready', createWindow)
